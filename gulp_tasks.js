e7ef347a8d78990a49014781e2c41e181458080e
tymlez <michael.reh@tymlez.com>
2017-01-18 18:39:37 +0100

    Update gulp_tasks.js

--- gulp_tasks.js
+++ gulp_tasks.js
@@ -209,4 +209,13 @@ module.exports = (gulp, options) => {
       }))
       .pipe(eslint.failAfterError())
   );
+  
+  var jsdoc = require('gulp-jsdoc3');
+  
+  gulp.task('doc', function (cb) {
+    var config = require('./jsdoc.json');
+    gulp.src(['README.md', './src/**/*.js'], {read: false})
+        .pipe(jsdoc(config, cb));
+});
+  
 };
